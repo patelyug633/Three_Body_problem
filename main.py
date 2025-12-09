022a7d7258935f1277256e1534264d3923c68eee
patelyug633 <patelyug633@gmail.com>
2025-12-08 23:15:28 -0500

    adding button fucntionality

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 G = 0.1     #Modified gravitational constant
 Screen_width, Screen_height = 640, 480
 bodies = []
+mass_increment = 100
+velocity_increment = 10
 class Body:
     def __init__(self, mass, position, momentum, radius, color):
         self.mass = mass
@@ -44,23 +46,45 @@ class Body:
     def traceOrbit(self):
         print("tracing orbit")
     
+    def getMassLable(self):
+        return f'Mass: {self.mass}'
+
+def scree_loop():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            return False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            if ifnear(x, y, 100, 400, 50):
+                print("Clicked on first body")
+            elif ifnear(x, y, 200, 400, 50):
+                print("Clicked on second body")
+            elif ifnear(x, y, 300, 400, 50):
+                print("Clicked on third body")
+    return True
+
+def ifnear(x, y, obj_x, obj_y, radius):
+    distance = np.sqrt((x - obj_x)**2 + (y - obj_y)**2)
+    return distance <= radius
 pygame.init()
 screen = pygame.display.set_mode((Screen_width, Screen_height), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 base_font = pygame.font.Font(None, 24)
 running = True
-user_mass = ''
+user_mass = 'mass'
 user_velocity = ''
 
 while running:
     dt = clock.tick(60) / 1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            running = False
-    text_surface = pygame.render(user_mass, True, (255, 255, 255))
-    # for body in bodies:
-    #     body.update(bodies, dt)
-    #     body.draw(screen)
-    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, (0, 255, 0), (Screen_width // 6, Screen_height - 100),50)
+    pygame.draw.circle(screen, (0, 255, 255), (Screen_width // 4, Screen_height - 100),50)
+    pygame.draw.circle(screen, (255, 255, 0), (Screen_width // 2, Screen_height - 100),50)
+    running  = scree_loop()
+    text_surface = base_font.render(user_mass, True, (255, 255, 255))
+    screen.blit(text_surface, (Screen_width // 6, Screen_height - 100))
+    text_surface = base_font.render(user_velocity, True, (255, 255, 255))
+    screen.blit(text_surface, (Screen_width // 4, Screen_height - 100))
+    text_surface = base_font.render(user_velocity, True, (255, 255, 255))
+    screen.blit(text_surface, (Screen_width // 2, Screen_height - 100))
     pygame.display.flip()
     clock.tick(60)
