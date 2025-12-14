2f9521c59391cfca0e60c11270a24810c7d72543
patelyug633 <patelyug633@gmail.com>
2025-12-14 11:29:04 -0500

    \]n

--- config.py
+++ config.py
@@ -6,5 +6,3 @@ G = 6.67430e-11         # Real gravitational constant
 distance_scale = 1e6   # 1 pixel = 1,000,000 meters
 time_scale = 60*60*24  # 1 frame = 1 day
 F_dt = 1/60 # 1 frame = 1 second
-
-
